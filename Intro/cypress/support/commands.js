7806e279af860505bdc5467f48ae85f3a4779558
Michael Love <michael.love42@gmail.com>
2024-02-04 18:10:30 -0800

    4

--- Intro/cypress/support/commands.js
+++ Intro/cypress/support/commands.js
@@ -17,5 +17,5 @@ Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { 
 -- This is a dual command --
 Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
 
--- This will overwrite an existing command --
-Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
