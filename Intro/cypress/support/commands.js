22f61eeeb5545e8211bfe454873acdd221a68815
Michael Love <michael.love42@gmail.com>
2024-02-04 18:10:58 -0800

    6

--- Intro/cypress/support/commands.js
+++ Intro/cypress/support/commands.js
@@ -11,8 +11,8 @@
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 
--- This is a child command --
-Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
