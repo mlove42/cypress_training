5f716913d2541b5c6c9586d4f3b163d55a7ea6af
Michael Love <michael.love42@gmail.com>
2024-03-16 09:55:51 -0700

    3

--- Intro/cypress/integration/examples/test1.js
+++ Intro/cypress/integration/examples/test1.js
@@ -1,17 +1,17 @@
 //cypress - Spec
 
-// describe("My First Test Suite", () => {
-//     // test cases goes inside the function
-//     it("My FirstTest case", () => {
-//         //test step
-//         // "cy" is a global command which whill help you to invoke any Cypress company\
-//         // visit allows us to visit any website
-//         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-//     });
-// it("My Second case", () => {
-//     //test step
-// });
-// });
+describe("My First Test Suite", () => {
+    // test cases goes inside the function
+    it("My FirstTest case", () => {
+        //test step
+        // "cy" is a global command which whill help you to invoke any Cypress company\
+        // visit allows us to visit any website
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    });
+it("My Second case", () => {
+    //test step
+});
+});
 lat one for the house
 describe("my first test"),
     () => {
