1e8474b4756d6a0b6185f7ead3d1ad491d439139
Michael Love <michael.love42@gmail.com>
2024-03-16 09:56:31 -0700

    4

--- Intro/cypress/integration/examples/test1.js
+++ Intro/cypress/integration/examples/test1.js
@@ -12,13 +12,13 @@
 //     //test step
 // });
 // });
-lat one for the house
-describe("my first test"),
-    () => {
-        it("Does not do much!", () => {
-            expect(true).to.equal(true);
-        });
-    };
+// lat one for the house
+// describe("my first test"),
+//     () => {
+//         it("Does not do much!", () => {
+//             expect(true).to.equal(true);
+//         });
+//     };
 
 describe("My First Test", () => {
     it("Does not do much!", () => {
